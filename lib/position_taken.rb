dcf1f65b0bdd81cdefd3223cf57da7819d3350a5
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 01:19:10 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
   taken = nil
   if board[index] == " " || board[index] == "" || board[index] == nil
     taken = false
-  elsif
+  else
     taken true
   end
 end
