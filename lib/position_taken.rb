ea6e37d746b3a6bfc0c92a22e38d8d5247ed0d71
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 01:18:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
+  taken = nil
   if board[index] == " " || board[index] == "" || board[index] == nil
-    puts "false"
+    taken = false
   elsif
-    puts "true"
+    taken true
   end
 end
